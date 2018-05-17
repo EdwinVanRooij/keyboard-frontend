78c40e4af769692641f14e00b77cb12dc50281de
Edwin van Rooij <edwinvrooij@gmail.com>
2018-05-17 23:19:18 +0200

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './foundation.min.css';
+
+const style = {
+    textAlign: 'center',
+};
+
+class App extends Component {
+    render() {
+        return (
+            <div style={style}>
+                Test
+            </div>
+        );
+    }
+}
+
+export default App;
